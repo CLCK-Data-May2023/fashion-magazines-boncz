381ba4eef9bdeef9699daa476599a555b5171cc7
Ailene Bonczkowski <a.boncz@gmail.com>
2023-10-09 00:43:25 -0400

    filtered by magazine type to get correct results

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,7 +1,6 @@
 -- Add your sql here
-SELECT customers.customer_name AS Customer, PRINTF( "$%.2f",sum(subscriptions.price_per_month * subscriptions.subscription_length)) AS 'Amount Due' 
+SELECT customers.customer_name AS Customer, PRINTF("$%.2f",sum(subscriptions.price_per_month * subscriptions.subscription_length)) AS 'Amount Due' 
 FROM orders
-	JOIN customers ON orders.customer_id = customers.customer_id
-    JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
-		WHERE order_status = 'unpaid'
+	JOIN customers ON (orders.customer_id = customers.customer_id) AND orders.order_status = 'unpaid'
+    JOIN subscriptions ON (orders.subscription_id = subscriptions.subscription_id) AND subscriptions.description = 'Fashion Magazine'
 	GROUP BY Customer;
