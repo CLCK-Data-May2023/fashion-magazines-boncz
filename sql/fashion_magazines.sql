7c0dca3da4f1f96041af9f31509a9591a51a2c3b
Ailene Bonczkowski <a.boncz@gmail.com>
2023-10-08 23:59:56 -0400

    completed sql query for assignment

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,7 @@
 -- Add your sql here
+SELECT customers.customer_name AS Customer, sum(subscriptions.price_per_month * subscriptions.subscription_length) AS 'Amount Due' 
+FROM orders
+	JOIN customers ON orders.customer_id = customers.customer_id
+    JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+		WHERE order_status = 'unpaid'
+	GROUP BY Customer;
